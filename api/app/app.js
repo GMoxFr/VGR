3594e8ba196f85d2dbe78291a2849c80053ceb82
Tristan Nobre <nobretristan@gmail.com>
2025-02-03 15:45:18 +0100

    added debug infos

--- api/app/app.js
+++ api/app/app.js
@@ -5,6 +5,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body);
+    next();
+});
+
 const apiRouter = require('@router');
 apiRouter(app);
 
